3bb3c6a2e853379ef8b5a20d41691863f25c9ed3
JinxCade <76182826+JinxCade@users.noreply.github.com>
2024-10-26 16:37:54 -0500

    add

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 window.onload = () => {
          var headTag = document.getElementsByTagName('head')[0]
          const linkforCSSfile = document.createElement("link"); //How to load CSS files using JavaScript?
-         linkforCSSfile.href = 'purple.css'
-         linkforCSSfile.type = 'text/css'
-         linkforCSSfile.rel = 'stylesheet'
+         //linkforCSSfile.href = 'purple.css'
+         //linkforCSSfile.type = 'text/css'
+         //linkforCSSfile.rel = 'stylesheet'
          headTag.appendChild(linkforCSSfile);
          document.body.appendChild(headTag);
       };
